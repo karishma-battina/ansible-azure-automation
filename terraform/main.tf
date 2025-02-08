36f1bf999c61be3dcae555492aa560b3f00e6aad
Karishma Battina <karishmabattina123@gmail.com>
2025-02-08 09:34:14 -0500

    File structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,18 @@
 #main.tf
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "vm-automation-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vm-vnet"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# Add subnet, public IP, and NIC resources
