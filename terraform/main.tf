3ccd80d75bf23a82c75bd7004a6beab83e23176d
Karishma Battina <67629745+karishma-battina@users.noreply.github.com>
2025-02-11 00:55:28 +0000

    Network create with tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,29 @@
-#main.tf
-provider "azurerm" {
-  features {}
-}
-
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "vm-automation-rg"
-  location = "East US"
+  location = var.resource_group_location
+  name     = var.resource_group_name
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vm-vnet"
+# Virtual Network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${random_pet.prefix.id}-vnet"
   address_space       = ["10.0.0.0/16"]
-  resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Add subnet, public IP, and NIC resources
+# Subnet 1
+resource "azurerm_subnet" "my_terraform_subnet_1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+# Subnet 2
+resource "azurerm_subnet" "my_terraform_subnet_2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
